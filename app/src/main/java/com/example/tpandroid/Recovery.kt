8bb2beb69a65b83a6202ad910801db598e0244dc
antoinecoulon <a.antoinecoulon@gmail.com>
2025-03-12 12:04:12 +0100

    Update: theme

--- app/src/main/java/com/example/tpandroid/Recovery.kt
+++ app/src/main/java/com/example/tpandroid/Recovery.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,6 +23,7 @@ import com.example.tpandroid.ui.theme.Page
 import com.example.tpandroid.ui.theme.TpAndroidTheme
 import com.example.tpandroid.ui.theme.TpButton
 import com.example.tpandroid.ui.theme.TpTextField
+import com.example.tpandroid.ui.theme.WrapPadding
 
 class RecoveryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,14 +45,32 @@ fun RecoveryPage() {
             Text(
                 text = "Password Recovery",
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
-                fontSize = 32.sp, color = Color.LightGray,
+                fontSize = 32.sp, color = Color(0xFFFDDFD9),
                 textAlign = TextAlign.Center
             )
-            TpTextField(fieldText = "Email")
-            TpButton(buttonText = "Send me an email!")
             Spacer(modifier = Modifier.weight(1f))
+            WrapPadding {
+                TpTextField(
+                    fieldText = "Email",
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.mail),
+                            contentDescription = "Mail",
+                            tint = Color(0xFFFDDFD9)
+                        )
+                    })
+            }
+            WrapPadding {
+                TpButton(buttonText = "Send me an email !")
+            }
+            Spacer(modifier = Modifier.weight(2f))
             Text(
-                text = "Next time don't forget your password dude !"
+                text = "Next time don't forget your password dude !",
+                color = Color(0xFFFDDFD9),
+                textAlign = TextAlign.Center,
+                fontStyle = FontStyle.Italic,
+                fontSize = 20.sp,
+                lineHeight = 24.sp
             )
         }
     }
