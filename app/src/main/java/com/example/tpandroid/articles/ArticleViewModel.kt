4408c487416ff0a8f4ba3252731632b1c6b824cb
antoinecoulon <a.antoinecoulon@gmail.com>
2025-03-13 10:57:29 +0100

    Add: viewModel

--- app/src/main/java/com/example/tpandroid/articles/ArticleViewModel.kt
+++ app/src/main/java/com/example/tpandroid/articles/ArticleViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.tpandroid.articles
 
-class ArticleViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+/*
+LiveData and StateFlow can be used effectively in a ViewModel to manage and observe data,
+but the choice between them depends on your specific use case and requirements
+ */
+
+class ArticleViewModel : ViewModel() {
+    private val articleRepository = ArticleRepository()
+
+    val articlesLiveData = MutableLiveData<List<Article>>()
+
+    init {
+        articlesLiveData.value = articleRepository.getArticles()
+    }
+
+    fun addArticle(title: String, desc: String, imgPath: String? = null) {
+        articleRepository.addArticle(title, desc, imgPath)
+        articlesLiveData.value = articleRepository.getArticles()
+    }
 }
