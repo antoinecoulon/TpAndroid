a54e884a4212cb902d0ce16fe0d77b48857d00ae
antoinecoulon <a.antoinecoulon@gmail.com>
2025-03-13 08:57:24 +0100

    Remove: unused imports

--- app/src/main/java/com/example/tpandroid/ArticleScreen.kt
+++ app/src/main/java/com/example/tpandroid/ArticleScreen.kt
@@ -1,9 +1,5 @@
 package com.example.tpandroid
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
