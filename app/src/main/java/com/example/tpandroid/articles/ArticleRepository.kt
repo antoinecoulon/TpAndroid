24270f44b8511cb35c04f18eb14b96b7823f0bfc
antoinecoulon <a.antoinecoulon@gmail.com>
2025-03-13 10:49:57 +0100

    Add: add function

--- app/src/main/java/com/example/tpandroid/articles/ArticleRepository.kt
+++ app/src/main/java/com/example/tpandroid/articles/ArticleRepository.kt
@@ -1,11 +1,23 @@
 package com.example.tpandroid.articles
 
 class ArticleRepository {
+    private val articles = mutableListOf(
+        Article("Article1", "Un premier article", "img_24"),
+        Article("Article2", "Un deuxième article", "img_27"),
+        Article("Article3", "Un troisième article", "img_076")
+    )
+
     fun getArticles(): List<Article> {
-        return listOf(
-            Article("Article1", "Un premier article", "img_24"),
-            Article("Article2", "Un deuxième article", "img_27"),
-            Article("Article3", "Un troisième article", "img_076"),
+        return articles.toList()
+    }
+
+    fun addArticle(title: String, desc: String, imgPath: String? = null) {
+        // Créer une instance d'Article
+        val newArticle = Article(
+            title = title,
+            desc = desc,
+            imgPath = imgPath ?: "default_image"
         )
+        articles.add(newArticle)
     }
 }
