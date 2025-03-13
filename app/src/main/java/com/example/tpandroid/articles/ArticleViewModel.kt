5c558fe003b6e8aea4073b45efd12851829c13cf
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-13 13:23:20 +0100

    Clean: comment

--- app/src/main/java/com/example/tpandroid/articles/ArticleViewModel.kt
+++ app/src/main/java/com/example/tpandroid/articles/ArticleViewModel.kt
@@ -4,11 +4,6 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-/*
-LiveData and StateFlow can be used effectively in a ViewModel to manage and observe data,
-but the choice between them depends on your specific use case and requirements
- */
-
 class ArticleViewModel : ViewModel() {
     private val _articles = MutableStateFlow<List<Article>>(emptyList())
     val articles: StateFlow<List<Article>> = _articles
