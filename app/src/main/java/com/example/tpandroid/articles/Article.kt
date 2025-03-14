586cea76c0db59a0b758b0616b47b840a31f3c9a
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-14 09:35:53 +0100

    Update Article.kt

--- app/src/main/java/com/example/tpandroid/articles/Article.kt
+++ app/src/main/java/com/example/tpandroid/articles/Article.kt
@@ -1,11 +1,11 @@
 package com.example.tpandroid.articles
 
 data class Article(
-    var id: Int,
-    var title: String,
-    var desc: String,
-    var author: String,
-    var imgPath : String?
+    val id: Int,
+    val title: String,
+    val desc: String,
+    val author: String,
+    val imgPath : String?
 ) {
 
 }
