af0e917dc39c91542b59a116c3a64b06e2a14b6c
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-14 09:34:47 +0100

    Add properties

--- app/src/main/java/com/example/tpandroid/articles/Article.kt
+++ app/src/main/java/com/example/tpandroid/articles/Article.kt
@@ -1,8 +1,10 @@
 package com.example.tpandroid.articles
 
 data class Article(
+    var id: Int,
     var title: String,
     var desc: String,
+    var author: String,
     var imgPath : String?
 ) {
 
